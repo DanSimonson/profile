f56238adf6d0d8407a0d4afc4d2a4fe6dccc2fc0
DanSimonson <dansimonson@mariposaweb.net>
2023-09-11 12:20:09 -0500

    flipping card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,49 +1,44 @@
 import React, { useState, useEffect } from "react";
+import ReactCardFlip from "react-card-flip";
 import "./Card.scss";
 import data from "../../data";
 
 export default function SingleCard() {
-  const [card, setCard] = useState(null);
   const [flip, setFlip] = useState(false);
-  const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null);
 
-  useEffect(() => {}, []);
-
-  const handleChoice = (flip) => {
-    console.log("flip: ", flip);
-    // choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-    // if (card === choiceOne || card === choiceTwo) {
-    //   console.log("choiceOne ==== :", choiceOne);
-    //   console.log("choiceTwo ==== :", choiceTwo);
-    // }
-  };
   const handleClick = () => {
+    console.log("clicking");
     setFlip(!flip);
-    handleChoice(flip);
   };
 
   return (
-    <div className="card" onClick={handleClick}>
-      <div className={flip ? "flip" : ""}>
-        <div className="front">
-          <h3>Front End developer</h3>
-          <p>
-            I have many Years of experince using JavaScript, React, Vue and CSS
-            to create beautiful websites and UI elements for my clients
-          </p>
-          <p className="more">Click To See More</p>
-        </div>
-        <div className="back">
-          <h3>Back End Developer</h3>
-          <p>
-            I am a full-stack developer skilled in the use of Node, MongoDB, and
-            Express. I have used my knowledge to help many clients who needed
-            help creating and consuming API's
-          </p>
-          <p className="more">Click To See More</p>
-        </div>
+    <ReactCardFlip
+      isFlipped={flip}
+      flipDirection="vertical"
+      flipSpeedBackToFront="2"
+      flipSpeedFrontToBack="2"
+    >
+      <div className="card__body">
+        <h3>Front End developer</h3>
+        <p>
+          I have many Years of experince using JavaScript, React, Vue and CSS to
+          create beautiful websites and UI elements for my clients
+        </p>
+        <p className="more" onClick={handleClick}>
+          Click To See More
+        </p>
       </div>
-    </div>
+      <div className="card__body">
+        <h3>Back End Developer</h3>
+        <p>
+          I am a full-stack developer skilled in the use of Node, MongoDB, and
+          Express. I have used my knowledge to help many clients who needed help
+          creating and consuming API's
+        </p>
+        <p className="more" onClick={handleClick}>
+          Click To See More
+        </p>
+      </div>
+    </ReactCardFlip>
   );
 }
