2d951129a9900e3fb673960cbb5e191b94543566
DanSimonson <dansimonson@mariposaweb.net>
2023-09-10 21:25:15 -0500

    added text

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -11,9 +11,22 @@ export default function SingleCard() {
   return (
     <div className="card">
       <div className={flipped ? "flipped" : ""}>
-        <div className="front">hello Front</div>
+        <div className="front">
+          <h3>Front End developer</h3>
+          <p>
+            I have many Years of experince using JavaScript, React, Vue and CSS
+            to create beautiful websites and UI elements for my clients
+          </p>
+          <p className="more">Click To See More</p>
+        </div>
         <div className="back" onClick={handleClick}>
-          Helo Back
+          <h3>Back End Developer</h3>
+          <p>
+            I am a full-stack developer skilled in the use of Node, MongoDB, and
+            Express. I have used my knowledge to help many clients who needed
+            help creating and consuming API's
+          </p>
+          <p className="more">Click To See More</p>
         </div>
       </div>
     </div>
