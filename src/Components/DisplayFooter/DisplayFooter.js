e66d851a67026e4e8f2d0653c94cef67f84c2aea
DanSimonson <dansimonson@mariposaweb.net>
2023-09-13 23:55:02 -0500

    added blog

--- src/Components/DisplayFooter/DisplayFooter.js
+++ src/Components/DisplayFooter/DisplayFooter.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./DisplayFooter.scss";
+import { NavLink } from "react-router-dom";
 
 const DisplayFooter = () => {
   return (
     <footer className="display-footer-basic-centered">
       <p className="display-footer-company-motto">
         Contact: dansimonson@mariposaweb.net
+      </p>
+      <p className="display-footer-links">
+        <NavLink to="">My Blog</NavLink>
       </p>
       <p className="display-footer-company-name">mariposaweb.net &copy; 2021</p>
     </footer>
