216f43da762534e30c09e6ae38b318e977ac6c83
DanSimonson <dansimonson@mariposaweb.net>
2023-09-14 23:19:58 -0500

    added an external link to blog

--- src/Components/DisplayFooter/DisplayFooter.js
+++ src/Components/DisplayFooter/DisplayFooter.js
@@ -3,16 +3,26 @@ import "./DisplayFooter.scss";
 import { NavLink } from "react-router-dom";
 
 const DisplayFooter = () => {
+  let linkBlog = "heuristic-neumann-ad864a.netlify.app";
   return (
-    <footer className="display-footer-basic-centered">
-      <p className="display-footer-company-motto">
-        Contact: dansimonson@mariposaweb.net
-      </p>
-      <p className="display-footer-links">
-        <NavLink to="">My Blog</NavLink>
-      </p>
-      <p className="display-footer-company-name">mariposaweb.net &copy; 2021</p>
-    </footer>
+    <>
+      <footer className="display-footer-basic-centered">
+        <p className="display-footer-company-motto">
+          Contact: dansimonson@mariposaweb.net
+        </p>
+        <p className="display-footer-links">
+          <a
+            href="https://heuristic-neumann-ad864a.netlify.app"
+            target="_blank"
+          >
+            My Blog
+          </a>
+        </p>
+        <p className="display-footer-company-name">
+          mariposaweb.net &copy; 2021
+        </p>
+      </footer>
+    </>
   );
 };
 
